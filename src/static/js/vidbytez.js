0a01e9e88fadc416dfc50ff38aa1da7971baa34e
Trigex <trigexss@gmail.com>
2018-10-29 19:50:36 -0700

    relationship ended with vidbytez, good code is my new best friend

--- src/static/js/vidbytez.js
+++ src/static/js/vidbytez.js
@@ -1,9 +1,8 @@
 /*   Vidbytez
  *   (c) Trigex 2018
  *   Licensed under the MIT License
-*/
+ */
 
-// probably ghetto and insecure way of storing the api key, but fuck yeah cookies!!!!!
 function createCookie(key, value, date) {
     var expiration = new Date(date).toUTCString();
     var cookie = escape(key) + "=" + escape(value) + ";expires=" + expiration + ";";
@@ -31,6 +30,10 @@ function deleteCookie(name) {
     createCookie(name, "", -1);
 }
 
+function isEmptyOrSpaces(str) {
+    return str === null || str.match(/^ *$/) !== null
+}
+
 // self explanitory fucknut
 function convertFormDataToJson(form) {
     return _.object($(form).serializeArray().map(function (v) {
@@ -50,19 +53,49 @@ function createNotification(content, type) {
 
 /* FORM EVENTS MOTHERFUCKER!!!! */
 
-// user login (/login)
 $("#user_login").submit((event) => {
     event.preventDefault();
 
     var formJson = convertFormDataToJson("#user_login");
-    userLogin(formJson);
+    ajaxRequest("/api/user/login", "POST", {
+        username: formJson.username,
+        password: formJson.password,
+        create_session: true
+    }, (response) => {
+        if (response.error) {
+            createNotification(response.error, "error");
+        } else {
+            // store authkey as a cookie
+            createCookie("authKey", response.authKey, Date.UTC(2020, 1, 1));
+            createNotification(response.success, "success");
+
+            setTimeout(() => {
+                window.location = "/home"
+            }, 1000);
+        }
+    });
 });
 
 $("#user_register").submit((event) => {
     event.preventDefault();
 
     var formJson = convertFormDataToJson("#user_register");
-    userRegister(formJson);
+    ajaxRequest("/api/user/create", "POST", {
+        username: formJson.username,
+        password: formJson.password,
+        email: formJson.email
+
+    }, (response) => {
+        if (response.error) {
+            createNotification(response.error, "error");
+        } else {
+            createNotification(response.success, "success");
+
+            setTimeout(() => {
+                window.location = "/login"
+            }, 2000)
+        }
+    });
 });
 
 $("#logout").click(() => {
@@ -74,7 +107,7 @@ $("#logout").click(() => {
         },
         success: (serverResponse) => {
             var response = JSON.parse(serverResponse);
-            if(response.error) {
+            if (response.error) {
                 createNotification(response.error, "error");
             } else {
                 deleteCookie("authKey");
@@ -87,61 +120,6 @@ $("#logout").click(() => {
         }
     });
 });
-
-
-
-/* AJAX!!! */
-function userRegister(registerFormJson) {
-    $.ajax({
-        url: "/api/user/create",
-        type: "POST",
-        data: {
-            username: registerFormJson.username,
-            password: registerFormJson.password,
-            email: registerFormJson.email
-        },
-        success: (serverResponse) => {
-            var response = JSON.parse(serverResponse);
-            if(response.error) {
-                createNotification(response.error, "error");
-            } else {
-                createNotification(response.success, "success");
-
-                setTimeout(() => {
-                    window.location = "/login"
-                }, 2000)
-            }
-        }
-    });
-}
-
-function userLogin(loginFormJson) {
-    $.ajax({
-        url: "/api/user/login",
-        type: "POST",
-        data: {
-            username: loginFormJson.username,
-            password: loginFormJson.password,
-            create_session: true
-        },
-        success: (serverResponse) => {
-            var response = JSON.parse(serverResponse);
-            if(response.error) {
-                createNotification(response.error, "error");
-            } else {
-                // store authkey as a cookie
-                createCookie("authKey", response.authKey, Date.UTC(2020, 1, 1));
-                createNotification(response.success, "success");
-
-                setTimeout(() => {
-                    window.location = "/home"
-                }, 1000)
-            }
-        }
-    });
-}
-
-/* STOLEN VIDLII JS ALERT ALERT LOL */
 
 // on new upload from (/upload)
 function new_upload() {
@@ -159,7 +137,7 @@ function new_upload() {
     // set page values
     $("#video_title").val(video_title);
     $("#video_title_header").html(video_title);
-    $("#custom_thumb_button").on("click mousedown", function() {
+    $("#custom_thumb_button").on("click mousedown", function () {
         if (!$(this).hasClass("loading")) {
             $("#custom_thumb_file").click();
             return !1
@@ -168,7 +146,7 @@ function new_upload() {
     $("#selectedFile").prop("disabled", !0);
     $("#uploader").animate({
         opacity: 0
-    }, 500, function() {
+    }, 500, function () {
         $("#old_upload_box").hide();
         $("#upload_select_box").show();
         $("#video_uploader").css("opacity", 0).animate({
@@ -179,37 +157,27 @@ function new_upload() {
 }
 
 function video_upload_check(video) {
-    $.ajax({
-        url: "/api/video/upload",
-        type: "POST",
-        data: {
-            action: "check",
-            filename: video.name,
-            filesize: video.size,
-            filetype: video.type,
-            authKey: readCookie("authKey")
-        },
-        success: function(serverResponse) {
-            var response = JSON.parse(serverResponse);
-            if(response.error) {
-                createNotification(response.error, "error");
-            } else {
-                createNotification(response.success, "success");
-                var videoID = response.videoID;
-                // create hidden videoID element on page, used for updating metadata
-                $("#video_progress").append("<p hidden id='videoid'>" + videoID + "</p>");
-                // go on, upload it motherfucker!
-                upload_video(video, videoID);
-            }
-        },
-        error: function() {
-            u("An error has occurred. Upload aborted!", !0)
+    ajaxRequest("/api/video/upload", "POST", {
+        action: "check",
+        filename: video.name,
+        filesize: video.size,
+        filetype: video.type,
+        authKey: readCookie("authKey")
+    }, (response) => {
+        if (response.error) {
+            createNotification(response.error, "error");
+        } else {
+            createNotification(response.success, "success");
+            var videoID = response.videoID;
+            // create hidden videoID element on page, used for updating metadata
+            $("#video_progress").append("<p hidden id='videoid'>" + videoID + "</p>");
+            // go on, upload it motherfucker!
+            upload_video(video, videoID);
         }
     });
 }
 
 function upload_video(video, videoID) {
-    console.log("upload started");
     // video upload ajax (harder to do through jQuery lol kill me)
     var formData = new FormData();
     formData.append("video_file", video);
@@ -243,30 +211,41 @@ function videoAbortHandle(event) {
     createNotification("The upload was aborted!", "error");
 }
 
-function isEmptyOrSpaces(str) {
-    return str === null || str.match(/^ *$/) !== null
-}
-
 function save_video_changes() {
     var description = $("#video_description").val();
     var tags = $("#video_tags").val();
     var title = $("#video_title").val();
     var videoID = $("#videoid").text();
+
+    ajaxRequest("/api/video/update", "POST", {
+        title: title,
+        description: description,
+        tags: tags,
+        videoID: videoID,
+        authKey: readCookie("authKey")
+    });
+}
+
+// wrapper around AJAX so I don't have to write the same boilerplate 10000000000 times
+function ajaxRequest(url, type, data, success_cb) {
     $.ajax({
-        url: "/api/video/update",
-        type: "POST",
-        data: {
-            title: title,
-            description: description,
-            tags: tags,
-            videoID: videoID,
-            authKey: readCookie("authKey")
-        },
-        success: function(serverResponse) {
+        url: url,
+        type: type,
+        data: data,
+        success: function (serverResponse) {
             var response = JSON.parse(serverResponse);
-            if(response.success) {
-                createNotification("The video information was updated!", "success");
+            if (success_cb) {
+                success_cb(response);
+            } else {
+                if (response.success) {
+                    createNotification(response.success, "success");
+                } else if (response.error) {
+                    createNotification(response.error, "error");
+                }
             }
+        },
+        error: function (err) {
+            createNotification(err, "error");
         }
-    })
+    });
 }
